a002471832ebf5aab97b531f375d84755435272f
niofrequency <124836368+niofrequency@users.noreply.github.com>
2026-08-15 15:58:42 +0900

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import AdminPage from './admin/AdminPage';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
+const isAdminRoute = window.location.pathname.replace(/\/+$/, '') === '/admin';
+
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    {isAdminRoute ? <AdminPage /> : <App />}
   </React.StrictMode>
 );
